5f3b1ed288ace14a933e1a33db6cc4595853f1a1
zahur <zahurmeerun@hotmail.com>
2021-11-03 12:24:11 +0400

    basic digital clock

--- digitalClock.py
+++ digitalClock.py
@@ -1,5 +1,18 @@
 from tkinter import Tk, Label
+import time
+import sys
 
 master = Tk()
+master.title('Digital Clock')
+
+def get_time():
+    time_var = time.strftime("%H:%M:%S %p")
+    clock.config(text=time_var)
+    clock.after(200, get_time)
+   
+
+clock = Label(master, font=("Calibri", 30), bg="grey", fg="white")
+clock.pack()
+get_time()
 
 master.mainloop()
